d7baf83d5ea0963435596d192b784923807df16e
Esha Patel <Patellesha11@gmail.com>
2025-04-14 12:21:00 +0300

    added quote

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -5,5 +5,6 @@ const quotes = [
     "Success doesn't just find you. You have to go out and get it.",
     "Don't let yesterday take up too much of today.",
     "If you are working on something exciting, it will keep you motivated.",
+    "Great things never come from comfort zones.",
        
 ]
