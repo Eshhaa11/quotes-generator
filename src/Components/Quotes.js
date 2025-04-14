b8d26f92baa2444a511453015fb6f9419bce8646
Esha Patel <Patellesha11@gmail.com>
2025-04-14 12:35:07 +0300

    added p

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -37,6 +37,7 @@ const quotes = [
     return (
         <div className="quote-cont">
            <h1>Todays Quote:</h1> 
+           <p className="quote-text">{quote}</p>
         </div>
     )
     
