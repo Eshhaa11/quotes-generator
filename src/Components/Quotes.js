5602bb4aae3ed3ce50e98dc3605f733fdc1c3146
Esha Patel <Patellesha11@gmail.com>
2025-04-14 12:30:11 +0300

    added quote

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -21,5 +21,6 @@ const quotes = [
     "Success usually comes to those who are too busy to be looking for it.",
     "The sacrifices parents make go unnoticed until one day, they don't — and everything makes sense.",
      "First, solve the problem. Then, write the code.",
+      "Success is the sum of small efforts repeated day in and day out.",
        
 ]
