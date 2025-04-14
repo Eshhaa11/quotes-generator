265694e31f78a2e36ac9524cdde0a3520cc8ef4c
Esha Patel <Patellesha11@gmail.com>
2025-04-14 12:20:29 +0300

    added quote

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -4,5 +4,6 @@ import './Quotes.css';
 const quotes = [
     "Success doesn't just find you. You have to go out and get it.",
     "Don't let yesterday take up too much of today.",
+    "If you are working on something exciting, it will keep you motivated.",
        
 ]
