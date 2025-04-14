8831c42f10e64040fbdc3627674c579456a4eb16
Esha Patel <Patellesha11@gmail.com>
2025-04-14 12:33:48 +0300

    added const random

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -30,7 +30,7 @@ const quotes = [
     const[quote, setQuote] = useState[quotes[0]];
 
     const generateQuote = () => {
-        
+        const randomIndex = Math.floor(Math.random() * quotes.length);
     }
     
  }
