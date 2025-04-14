15936e349d3908be5791ed78d63323fa2af67679
Esha Patel <Patellesha11@gmail.com>
2025-04-14 12:28:33 +0300

    added quote

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -17,6 +17,7 @@ const quotes = [
     "Sometimes later becomes never. Do it now.",
     "Don't watch the clock; do what it does. Keep going.",
     "Code is like humor. When you have to explain it, it's bad.",
-     "Success usually comes to those who are too busy to be looking for it.",
+    "Success usually comes to those who are too busy to be looking for it.",
+    "The sacrifices parents make go unnoticed until one day, they don’t — and everything makes sense.",
        
 ]
